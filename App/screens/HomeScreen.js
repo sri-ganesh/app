55a67afac7a6dc70ccde9b21a9fa1b667245433b
Shauneoo <contact@shaune.rocks>
2017-05-11 11:25:25 +0100

    WIP User API request

--- App/screens/HomeScreen.js
+++ App/screens/HomeScreen.js
@@ -12,6 +12,8 @@ export default class HomeScreen extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      username: 'antero.duarte',
+      password: 'testpassword',
       firstTimeWelcome: true,
       activeSection: false,
       collapsed: true,
@@ -21,6 +23,45 @@ export default class HomeScreen extends React.Component {
       sliderValue: 0.3,
       disableGestures: true
     };
+  }
+
+  componentWillMount() {
+
+    fetch('https://activexbacks.com/api/user/token', {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+         'user': this.state.username,
+         'pass': this.state.password
+      })
+    }).then( (userToken) => {
+      fetch('https://activexbacks.com/api/user/login', {
+        method: 'POST',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          'name': this.state.username,
+          'pass': this.state.password,
+          'token': JSON.parse(userToken._bodyText).token
+        })
+      }).then( (userData) => {
+        if(userData.status === 200){
+          alert(JSON.stringify(userData));
+        }
+        else{
+          alert('Login Failed. Please try again');
+        }
+      }).catch((error) => {
+        alert('Login Failed. Please try again'+error);
+      });
+    }).catch((error) => {
+      alert('User authentication failed for reason: '+error);
+    });
   }
 
   render() {
